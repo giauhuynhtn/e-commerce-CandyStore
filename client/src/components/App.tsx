2841ceba8d079797f5888ca345a55a52b7bae454
Giau Huynh <giau.huynh@tuni.fi>
2022-09-27 14:31:13 +0300

    add byCategory thunk, remove 'any' type in slices

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+
         <Route path='/:productId' element={<ProductInfo />} />
       </Routes>
     </BrowserRouter>
