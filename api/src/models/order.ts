eb811ffaf3b523fefba87c87135a1041147f8273
Giau Huynh <giau.huynh@tuni.fi>
2022-10-16 21:23:18 +0300

    fixed enum type in Order model

--- api/src/models/order.ts
+++ api/src/models/order.ts
@@ -37,7 +37,7 @@ const orderSchema = new mongoose.Schema({
     ref: 'User',
   },
   status: {
-    type: String,
+    enum: ['pending', 'delivered', 'returned', 'cancelled'],
     default: 'pending',
   },
 })
